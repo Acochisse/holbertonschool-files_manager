571ee2906c8e3fe50931a5459bf5f1c5c112e5d8
Acochisse <1819@holbertonschool.com>
2022-11-16 19:01:58 -0600

    changes to names of variables and logic of connection to database in class construction

--- utils/db.js
+++ utils/db.js
@@ -1,33 +1,36 @@
-const { promisify } = require('promisify');
+const { promisify } = require('util');
 const mongodb = require('mongodb');
 const { MongoClient } = require('mongodb');
 
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 27017 ;;
-const DB_DATABASE =  process.env.DATABASE || "file_manager";
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017 ;;
+const database =  process.env.DATABASE || "file_manager";
 const url = `mongodb://${host}:${port}/${database}`;
 
 class DbClient {
   constructor() {
-    this.host = DB_HOST;
-    this.port = DB_PORT;
-    this.database = DB_DATABASE;
-    this.client = new MongoClient(url);
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.client.connect((err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+    
   }
 
+
   isAlive() {
-    console.log(this.client);
     return this.client.isConnected();
   }
 
   async nbUsers() {
-    this.client.use(DB_DATABASE);
+    this.client.use(database);
     const UserPromise = promisify(this.client.users.find()).bind(this.client);
     return UserPromise();
   }
 
   async nbFiles() {
-    this.client.use(DB_DATABASE);
+    this.client.use(database);
     const FilePromise = promisify(this.client.files.find()).bind(this.client);
     return FilePromise();
   }
