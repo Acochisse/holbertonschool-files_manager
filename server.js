d9a6cc8be145811367a385b9a020c572e2c8a25e
Acochisse <1819@holbertonschool.com>
2022-11-17 15:18:49 -0600

    imported routes correctly?

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const redisClient = require('./utils/redis');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const routePath = require('routes/index');
+const router = require('./routes/index');
 const { Server } = require('mongodb/lib/core');
+
+app.use(router);
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 }
 );
-
-app.run = function() {
-  app.get(routePath);
-}
 
 modules.exports = app;
