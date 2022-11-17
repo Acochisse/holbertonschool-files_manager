d2ffd03d8aa1ca99388ed25bbfddeac187d4a180
Acochisse <1819@holbertonschool.com>
2022-11-17 15:12:06 -0600

    adding server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const redisClient = require('./utils/redis');
+const app = express();
+const port = process.env.PORT || 5000;
+
+const routePath = require('routes/index.js');
+const { Server } = require('mongodb/lib/core');
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+}
+);
+
+app.run = function() {
+  app.get(routePath);
+}
+
+modules.exports = app;
