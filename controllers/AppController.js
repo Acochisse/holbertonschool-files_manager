7f594a31cd38728e45c4b53f1b7f8ad269c2f2c3
Acochisse <1819@holbertonschool.com>
2022-11-17 15:22:17 -0600

    added correct linking back to server

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,7 +1,14 @@
-const app = require('server.js');
+const app = require('./server.js');
 const routePath = require('.routes/index.js');
 const redisClient = require('./utils/redis');
 const mongoClient = require('./utils/db');
 
-app.get('/status'
-)
+app.get('/status', (req, res) => { {
+    res.status(200).send(JSON.stringify({ "redis": redisClient.isAlive(), "db": mongoClient.isAlive() }));
+  }
+});
+
+app.get('/stats', (req, res) => {
+  res.status(200).send(JSON.stringify({ "users": db.dbClient.nbUsers(), "files": db.dbClient.nbFiles() }));
+});
+
