f1683b826464700fcc0abaed8c36d8d6b440dd4b
AJS0191 <ashtonjamess@live.com>
2022-11-18 14:40:50 -0600

    yay s

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 
 const host = process.env.MONGO_HOST || 'localhost';
 const port = process.env.MONGO_PORT || 27017 ;
-const database =  process.env.MONGO_DB || "file_manager";
+const database =  process.env.MONGO_DB || "files_manager";
 const url = `mongodb://${host}:${port}`;
 
 class DbClient {
