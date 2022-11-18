c9dd577300f5918b7760597a9a964175bc91454a
AJS0191 <ashtonjamess@live.com>
2022-11-17 19:00:22 -0600

    change routes in index to take old function format

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,8 @@ const express = require('express');
 const router = express.Router();
 const {getStats, getStatus} = require('../controllers/AppController')
 
-router.get('/status', (req, res) => {
-  res.send(getStatus());
-})
+router.get('/status', getStatus);
 
-router.get('/stats', (req, res) => {
-  res.send(getStats());
-})
+router.get('/stats', getStats);
 
 module.exports = router
