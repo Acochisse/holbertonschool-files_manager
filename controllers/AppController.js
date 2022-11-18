1771e23baa6eea73110af8dacfe947cb2dbf3efa
AJS0191 <ashtonjamess@live.com>
2022-11-17 18:47:56 -0600

    remove new

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,7 +2,7 @@ import redisClient from "../utils/redis";
 import dbClient from "../utils/db";
 
 
-new class AppController {
+class AppController {
   async getStatus() {
     const data = JSON.stringify({
     'redis': redisClient.isAlive(),
