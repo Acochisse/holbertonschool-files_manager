c5c4335f9b3719c323fafd0137eb77ca45c1209f
AJS0191 <ashtonjamess@live.com>
2022-11-18 17:58:53 -0600

    send status

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -9,7 +9,7 @@ module.exports = new class AppController {
     'db': dbClient.isAlive(),
   })
   console.log(data);
-  res.send(data);
+  res.status(200).send(data);
   }
 
   async getStats(req, res) {
@@ -17,7 +17,7 @@ module.exports = new class AppController {
     'users': await dbClient.nbUsers(),
     'files': await dbClient.nbFiles(),
   })
-  res.send(data)
+  res.status(200).send(data)
   }
 }
 
