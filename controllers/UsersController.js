72c29797b492ecc51d21315dbf6fbe9172457f59
AJS0191 <ashtonjamess@live.com>
2022-11-18 10:03:30 -0600

    add new post route for users.

---
+++ controllers/UsersController.js
@@ -0,0 +1,22 @@
+import redisClient from "../utils/redis";
+import dbClient from "../utils/db";
+const SHA1 = require('sha1');
+const hash = SHA1.create()
+
+modules.exports = new class UsersController {
+  async getUser(req, res) {
+    if (!req.params.email) {
+      res.status(400).send(new Error('Missing email'));
+    }
+    if (!req.params.password) {
+      res.status(400).send(new Error('Missing password'));
+    }
+    users = dbClient.users;
+    if (users.findOne(req.params.email)) {
+      res.status(400).send(new Error('Already exist'));
+    }
+    const pass = hash(req.params.password);
+    newUser = {email:req.params.email, password:pass};
+    res.status(201).send(JSON.stringify(newUser))
+    };
+  };
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 const {getStats, getStatus} = require('../controllers/AppController')
-
+const { getUser } = require('../controllers/UsersController')
 router.get('/status', getStatus);
 
 router.get('/stats', getStats);
+
+router.post('/users', getUser);
 
 module.exports = router
