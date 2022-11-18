8f7f590fcd94d95ffbf2c74f07856727520a9cc5
AJS0191 <ashtonjamess@live.com>
2022-11-18 11:03:01 -0600

    rework to get info from body

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,19 +4,18 @@ const SHA1 = require('sha1');
 
 module.exports = new class UsersController {
   async getUser(req, res) {
-    console.log(req);
-    if (!req.params.email) {
+    if (!req.body.email) {
       res.status(400).send(new Error('Missing email'));
     }
-    if (!req.params.password) {
+    if (!req.body.password) {
       res.status(400).send(new Error('Missing password'));
     }
     users = await dbClient.users;
-    if (users.findOne(req.params.email)) {
+    if (users.findOne(req.body.email)) {
       res.status(400).send(new Error('Already exist'));
     }
-    const pass = hash(req.params.password);
-    newUser = {email:req.params.email, password:pass};
+    const pass = hash(req.body.password);
+    newUser = {email:req.body.email, password:pass};
     res.status(201).send(JSON.stringify(newUser))
     };
   };
