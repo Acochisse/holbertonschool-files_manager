00b93dd3de1afe9e3e1947605d589915fc719934
Acochisse <1819@holbertonschool.com>
2022-11-18 21:01:41 -0600

    changed .send for .json again

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -20,6 +20,6 @@ module.exports = new class UsersController {
     const pass = SHA1(req.body.password);
     const newUser = {email, password:pass};
     await dbClient.users.insertOne(newUser);
-    return res.status(201).send({ id: newUser.insertedId, email });
+    return res.status(201).json({ id: newUser.insertedId, email });
     };
   };
