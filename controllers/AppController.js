592e1db3db611570279cc9aaeaadbfe33af1f256
AJS0191 <ashtonjamess@live.com>
2022-11-18 19:02:46 -0600

    app controller dont jsonify

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,18 +4,18 @@ import dbClient from "../utils/db";
 
 module.exports = new class AppController {
   async getStatus(req, res) {
-    const data = JSON.stringify({
+    const data = {
     'redis': redisClient.isAlive(),
     'db': dbClient.isAlive(),
-  })
+  }
   res.status(200).send(data);
   }
 
   async getStats(req, res) {
-    const data = JSON.stringify({
+    const data = {
     'users': await dbClient.nbUsers(),
     'files': await dbClient.nbFiles(),
-  })
+  }
   res.status(200).send(data)
   }
 }
