fd8fb7dc70866c0f0e0adb193cb20a751f2b0ae6
Acochisse <1819@holbertonschool.com>
2022-11-19 10:03:03 -0600

    added AuthController v1

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,20 @@ const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController')
 const UsersController = require('../controllers/UsersController')
+const AuthController = require('../controllers/AuthController')
 
 router.get('/status', AppController.getStatus);
 
 router.get('/stats', AppController.getStats);
 
 router.post('/users', UsersController.getUser);
+
+router.post('/connect', AuthController.getConnect);
+
+router.delete('/disconnect', AuthController.getDisconnect);
+
+router.get('/files', AuthController.getFiles);
+
+
 
 module.exports = router;
