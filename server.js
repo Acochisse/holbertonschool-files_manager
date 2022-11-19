31ff75dcd6f6e1b5202c45f07dd50587ea8d52c9
AJS0191 <ashtonjamess@live.com>
2022-11-18 18:58:48 -0600

    give router a route to listen

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 
 const router = require('./routes/index');
 app.use(express.json());
-app.use(router);
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
