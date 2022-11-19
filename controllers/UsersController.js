6e65c4943b6c875e555f70a533f7e289008ecb2d
Acochisse <1819@holbertonschool.com>
2022-11-18 20:21:52 -0600

    errors are errored differently now

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,20 +1,21 @@
 import redisClient from "../utils/redis";
 import dbClient from "../utils/db";
+import { json } from "express";
 const SHA1 = require('sha1');
 
 module.exports = new class UsersController {
   async getUser(req, res) {
     const email = req.body.email;
     if (!req.body.email) {
-      res.status(400).send(new Error('Missing email'));
+      res.status(400).send(json({error: 'Missing email'}));
     }
     if (!req.body.password) {
-      res.status(400).send(new Error('Missing password'));
+      res.status(400).send(json({error: 'Missing password'}));
     }
     const users = await dbClient.users;
     const uArray = await users.find({email}).toArray();
     if (uArray.length >= 1) {
-      res.status(400).send(new Error('Already exist'));
+      res.status(400).send(json({error: 'User already exists'}));
     }
     const pass = SHA1(req.body.password);
     const newUser = {email, password:pass};
