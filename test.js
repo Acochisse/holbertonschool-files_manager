382635ea64bc494b44891c0c2cb7867cecf7a106
AJS0191 <ashtonjamess@live.com>
2022-11-18 18:03:23 -0600

    new test

--- test.js
+++ test.js
@@ -1,63 +1,19 @@
-import MongoClient from 'mongodb';
 import chai from 'chai';
-import dbClient from './utils/db.js';
+import chaiHttp from 'chai-http';
 
-console.log(`this is dbClient ${dbClient.db}`)
-console.log(`this is the collection users ${dbClient.db.collection('users')}`)
+chai.use(chaiHttp);
 
-
-describe('dbClient test', () => {  
-    let testClientDb = null;
-
-    beforeEach(async () => {
-        const dbInfo = {
-            host: process.env.DB_HOST || 'localhost',
-            port: process.env.DB_PORT || '27017',
-            database: process.env.DB_DATABASE || 'files_manager'
-        };
-        const mcPromise = () => {
-            return new Promise((resolve, reject) => {
-                MongoClient.connect(`mongodb://${dbInfo.host}:${dbInfo.port}/${dbInfo.database}`, (err, client) => {
-                    if (err) {
-                        reject();
-                    }
-                    else {
-                        resolve(client.db(dbInfo.database))
-                    }
-                });
-            }); 
-        };
-        testClientDb = await mcPromise();
-        await testClientDb.collection('users').deleteMany({})
-        await testClientDb.collection('users').insertOne({ email: "me@me.com" })
-
-        const waitConnection = () => {
-            return new Promise((resolve, reject) => {
-                let i = 0;
-                const repeatFct = async () => {
-                    await setTimeout(() => {
-                        i += 1;
-                        if (i >= 5) {
-                            reject()
-                        }
-                        else if(!dbClient.isAlive()) {
-                            repeatFct()
-                        }
-                        else {
-                            resolve()
-                        }
-                    }, 1000);
-                }
-                repeatFct();
-            })
-        };
-        await waitConnection();
-    });
-        
-    afterEach(async () => {
-    });
-
-    it('nbUsers for a collection with one document', async () => {
-        chai.assert.equal(await dbClient.nbUsers(), 1);  
-    })
+describe('GET /status', () => {
+    it('GET /status exists', (done) => {
+        chai.request('http://localhost:5000')
+            .get('/status')
+            .end((err, res) => {
+                chai.expect(err).to.be.null;
+                chai.expect(res).to.have.status(200);
+                const bodyJson = res.body;
+                chai.expect(bodyJson.redis).to.be.true;
+                chai.expect(bodyJson.db).to.be.true;
+                done();
+            });
+    }).timeout(30000);
 });
