bbd1e743916d0363a3a49e43d753b32319adb4b2
AJS0191 <ashtonjamess@live.com>
2022-11-18 18:50:20 -0600

    bring in the app controller class

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-const {getStats, getStatus} = require('../controllers/AppController')
+const AppController = require('../controllers/AppController')
 const { getUser } = require('../controllers/UsersController')
-router.get('/status', getStatus);
 
-router.get('/stats', getStats);
+router.get('/status', AppController.getStatus);
+
+router.get('/stats', AppController.getStats);
 
 router.all('/users', getUser);
 
