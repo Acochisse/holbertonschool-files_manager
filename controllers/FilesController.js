80d0a61a2d4ccfb614a21b886eacaa799842436c
Acochisse <1819@holbertonschool.com>
2022-11-19 12:58:36 -0600

    trying to nail down where parentId isn't working

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -13,18 +13,16 @@ module.exports = new class FilesController {
       return response.status(401).json({error: 'Unauthorized'});
     }
 
-    const { name, type, parentId, isPublic, data } = request.body;
+    const { name, type, parentId = 0, isPublic = false, data } = request.body;
     if (!name) return response.status(400).json({ error: 'Missing name' });
     if (!type) return response.status(400).json({ error: 'Missing type' });
-    if (!parentId) parentId = 0;
-    if (!isPublic) isPublic = false;
     if (!data && type !== 'folder') return response.status(400).json({ error: 'Missing data' });
-
     const files = await dbClient.files;
     const file = await dbClient.files.findOne({ _id: parentId });
-    if (parentId && !file) return response.status(400).json({ error: 'Parent not found' });
-    if (parentId && file.type !== 'folder') return response.status(400).json({ error: 'Parent is not a folder' });
-
+    if (parentID !== 0) {
+      if (!file) return response.status(400).json({ error: 'Parent not found' });
+      if (file.type !== 'folder') return response.status(400).json({ error: 'Parent is not a folder' });
+    }
     const fileObj = {
       userId: user,
       name,
