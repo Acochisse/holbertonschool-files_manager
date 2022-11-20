d8293ea4c26b516914065afde89c687b6347fe68
Acochisse <1819@holbertonschool.com>
2022-11-20 15:31:13 -0600

    dropped the 200 on the return for getShow

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -91,8 +91,9 @@ module.exports = new class FilesController {
       const dbID = new mongo.ObjectId(id)
       const file = await dbClient.files.findOne({ _id: dbID });
       if (!file) return res.status(404).json({ error: 'Not found' });
+      // 95 fixed the check for this 404 but broke the good response :(
       if (file.userId !== user) return res.status(404).json({ error: 'Not found' });
-      return res.status(200).json(file);
+      return res.json(file);
     }
 
     async getIndex(req, res) {
