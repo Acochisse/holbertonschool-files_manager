f9485e01f978da1aedb7a80cea933b40c75d60ac
Acochisse <1819@holbertonschool.com>
2022-11-21 18:06:20 -0600

    eslint

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 const router = express.Router();
-const AppController = require('../controllers/AppController')
-const UsersController = require('../controllers/UsersController')
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
 const FilesController = require('../controllers/FilesController');
 
